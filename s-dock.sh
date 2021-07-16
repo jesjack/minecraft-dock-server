57f64dc629321026c1f6c1b5ba2430600177af6d
Edgar Jesús Moreno Castañeda <jesjack25_03@hotmail.com>
2021-07-16 06:16:09 +0000

    G cloud

--- s-dock.sh
+++ s-dock.sh
@@ -1,5 +1,5 @@
 sudo docker exec mc rcon-cli stop
 sudo docker container stop $(sudo docker ps --filter "name=mc" -qa)
 sudo docker container rm $(sudo docker ps --filter "name=mc" -qa)
-sudo docker run -d -it -p 25565:25565 -e EULA=TRUE -e MEMORY=2G -e MAX_TICK_TIME=-1 -e ENABLE_AUTOPAUSE=TRUE -v /home/jesjack/Proyectos/minecraft/doc-serv-1:/data --name mc itzg/minecraft-server
-sudo notify-send 'Servidor de minecraft' 'Se a reinstalado el contenedor de minecraft "mc"'
+sudo docker run -d -it -p 25565:25565 -e EULA=TRUE -e MEMORY=2G -e MAX_TICK_TIME=-1 -e ENABLE_AUTOPAUSE=TRUE -v /home/jesjack25_03/minecraft-dock-server/doc-serv-1:/data --name mc itzg/minecraft-server
+# sudo notify-send 'Servidor de minecraft' 'Se a reinstalado el contenedor de minecraft "mc"'
